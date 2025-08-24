45c6a6eb2dc31612f660157fc452361745b67e6a
singhrkp <post2rkp@gmail.com>
2025-08-25 00:02:06 +0800

    Add unit test for InvestorIQ main function

---
+++ InvestorIQ/tests/test_main.py
@@ -0,0 +1,10 @@
+import unittest
+from InvestorIQ import main
+
+class TestInvestorIQMain(unittest.TestCase):
+    def test_main(self):
+        # This is a placeholder test
+        self.assertIsNone(main.main())
+
+if __name__ == "__main__":
+    unittest.main()
